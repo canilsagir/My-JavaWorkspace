5ad06951b261fca1a1f29098571c2edd04b3812c
Cüneyt Anıl Sağır <canilsagir@gmail.com>
2022-10-16 23:49:53 +0300

    conditionalsExample

---
+++ conditionals/Main.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi = 31;
+		 
+		if(sayi<25 ) {
+			
+			System.out.println("Kücük");
+		} else if (sayi>31) {
+			System.out.println("Selamlar");
+		}else {
+			System.out.println("Eşittir");
+		}
+	}
+
+}
