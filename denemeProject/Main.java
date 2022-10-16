8ab60f6498bc62117d8e208a9cf2dd3ac5f744c5
Cüneyt Anıl Sağır <canilsagir@gmail.com>
2022-10-16 23:52:09 +0300

    denemeProject

---
+++ denemeProject/Main.java
@@ -0,0 +1,28 @@
+package denemeProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String start = "Merhaba Dünya";
+		
+		int sayac = 0;
+		
+		for(int i =0; i<5; i++) {
+			if(sayac<5) {
+				
+				System.out.println(start);
+			}
+			
+		}
+		/*//while(sayac<=10) {
+			sayac++;
+			System.out.println("Merhaba Dünya");*/
+			
+		}
+		
+		
+		}
+
+	
+
+
