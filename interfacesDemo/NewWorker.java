0354fd07cf9e4cf18977f0e54a0cb8c9f26c6e61
Cüneyt Anıl Sağır <canilsagir@gmail.com>
2022-10-28 16:37:27 +0300

    interfacesDemo

---
+++ interfacesDemo/NewWorker.java
@@ -0,0 +1,11 @@
+package interfacesDemo;
+
+public class NewWorker implements IWorkable {
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ interfacesDemo/Worker.java
@@ -0,0 +1,24 @@
+package interfacesDemo;
+
+public class Worker implements IWorkable, IEatable, IPayable {
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maasOde() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
